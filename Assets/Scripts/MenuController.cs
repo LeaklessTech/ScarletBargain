4a70bf76d4af6e21a7e6174ba0ae87af5f7d3d0f
Michael Markoe <mmarkoe3@gatech.edu>
2025-09-13 08:04:36 -0700

    starting point for state of game intro - playing -quit

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class MenuController : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void QuitGameImmediate()
+    {
+
+    }
+
+    public void ShowQuitConfirm()
+    {
+
+    }
+
+    public void ConfirmQuit()
+    {
+
+    }
+
+    public void CancelQuit()
+    {
+        
+    }
+}
