6062119a979a2db6c1f3564d48db4bfc3fd1e578
JebLab <82290002+JebLab@users.noreply.github.com>
2025-10-26 13:42:30 -0500

    Setup outline for game manager

---
+++ Assets/Scripts/LevelGeneration/GameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        // Generate level
+        
+        // Spawn objects
+
+        // Spawn waypoints
+
+        // Spawn player/monster (start game)
+    }
+}
