0ccca81d853210aa6be1e556bd71fda4ad929806
Michael Markoe <mmarkoe3@gatech.edu>
2025-10-26 14:51:53 -0700

    added spawnable objects for helper class.

--- Assets/Scripts/LevelGeneration/GameManager.cs
+++ Assets/Scripts/LevelGeneration/GameManager.cs
@@ -2,14 +2,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public PropSpawner propSpawner;
     void Start()
     {
         // Generate level
-        
+
         // Spawn objects
+        // This assumes the level is fully generated
+        SpawnObjects();
 
         // Spawn waypoints
 
         // Spawn player/monster (start game)
+    }
+
+    void SpawnObjects()
+    {
+        if (propSpawner == null)
+        {
+            Debug.LogWarning("PropSpawner reference not set.");
+            return;
+        }
+
+        propSpawner.SpawnNow();
     }
 }
