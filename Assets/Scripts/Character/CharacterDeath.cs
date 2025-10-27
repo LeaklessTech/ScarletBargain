09fdebc58750f1eed684a4563466efc3e7ded763
Michael Markoe <mmarkoe3@gatech.edu>
2025-10-27 12:54:06 -0700

    added a one second delay for the user to see the death.

--- Assets/Scripts/Character/CharacterDeath.cs
+++ Assets/Scripts/Character/CharacterDeath.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class CharacterDeath : MonoBehaviour
 {
     public void TriggerDeath(Component sender, object data)
     {
         // Debug.Log("I GOT KILLED. this instance: " + this.gameObject.GetInstanceID() + " and the instance id passed in: " + ((int)data));
-        if(data is int)
+        if (data is int)
         {
             int id = (int)data;
+            StartCoroutine(DeathSequence(id));
 
-            if (id == this.gameObject.GetInstanceID())
-                this.gameObject.SetActive(false);
-
-            if (this.gameObject.tag == "Player")
-                SceneManager.LoadScene("LoseScene", LoadSceneMode.Single);
         }
     }
+    private IEnumerator DeathSequence(int id)
+    {
+        // just a little delay for the user to take it in
+        yield return new WaitForSeconds(1f);
+
+        if (id == this.gameObject.GetInstanceID())
+            this.gameObject.SetActive(false);
+
+        if (this.gameObject.tag == "Player")
+            SceneManager.LoadScene("LoseScene", LoadSceneMode.Single);
+
+    }
 }
+
+
