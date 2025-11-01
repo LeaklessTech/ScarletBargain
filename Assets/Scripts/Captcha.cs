08e27a12c284f0be116c3dc32d6ce6cadc993d3e
Truman Hess <hess2918@gmail.com>
2025-11-01 12:56:50 -0700

    feat: highlights toggle when clicked

--- Assets/Scripts/Captcha.cs
+++ Assets/Scripts/Captcha.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
@@ -8,18 +9,24 @@ public class Captcha : MonoBehaviour
 {
 
     System.Random rnd = new System.Random();
+    public Toggle[] ToggleList;
+
     private void Awake()
     {
         var invalidImages = Resources.LoadAll<Texture>("Images/Captcha/InvalidImages");
         var validImages = Resources.LoadAll<Texture>("Images/Captcha/ValidImages");
 
-        var toggles = this.transform.Find("Panel").gameObject.GetComponentsInChildren<UnityEngine.UI.Toggle>();
+        ToggleList = this.transform.Find("Panel").gameObject.GetComponentsInChildren<Toggle>();
 
         RawImage image1 = transform.Find("Panel/Image1").gameObject.GetComponent<RawImage>();
         image1.texture = Resources.Load<Texture>("Images/Captcha/InvalidImages/bicycle_0");
 
 
-        toggles.FirstOrDefault().onValueChanged.AddListener(delegate { f(); });
+        for(int i = 0; i < ToggleList.Length; i++)
+        {
+            int index = i;
+            ToggleList[index].onValueChanged.AddListener(delegate{ HighlightToggle(ToggleList[index]); });
+        }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -33,9 +40,28 @@ public class Captcha : MonoBehaviour
     {
 
     }
-    
-    private void f()
+
+    private void HighlightToggle(Toggle toggle)
     {
-        
+        if (toggle.isOn)
+        {
+            var l = toggle.colors;
+            l.normalColor = Color.blue;
+            l.selectedColor = Color.blue;
+            l.highlightedColor = Color.blue;
+            l.pressedColor = Color.blue;
+
+            toggle.colors = l;
+        }
+        else
+        {
+            var l = toggle.colors;
+            l.normalColor = Color.white;
+            l.selectedColor = Color.white;
+            l.highlightedColor = Color.white;
+            l.pressedColor = Color.white;
+
+            toggle.colors = l;
+        }
     }
 }
