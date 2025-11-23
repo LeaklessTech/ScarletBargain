25167f71cfe3d32eb3145bc6327ae99371aea48e
Michael Markoe <mmarkoe3@gatech.edu>
2025-11-22 18:44:42 -0600

    speed up the rotation

--- Assets/Scripts/Character/SpinPrisoner.cs
+++ Assets/Scripts/Character/SpinPrisoner.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 public class SpinPrisoner : MonoBehaviour
 {
     [Tooltip("Degrees per second around the spin axis.")]
-    public float rotationSpeed = 30f;
+    public float rotationSpeed = 50f;
 
     void Update()
     {
-        // Rotate around the chosen axis, at rotationSpeed degrees per second
         transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.Self);
     }
 }
